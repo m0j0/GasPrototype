a97ac6bd3a659d0e9a0a574cbed685f9bbaec48f
m0j0 <maslovn@gmail.com>
2018-07-03 19:11:46 +0300

    refactoring

---
+++ Prototype.Core.Model/Interfaces/IMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Windows.Input;
+
+namespace Prototype.Core.Interfaces
+{
+    public interface IMenu
+    {
+        string Header { get; }
+
+        IReadOnlyCollection<IMenuItem> Items { get; }
+    }
+
+    public interface IMenuItem
+    {
+        string Text { get; }
+
+        ICommand Command { get; }
+
+        IMenu Submenu { get; }
+    }
+}
---
+++ Prototype.Core.Model/Models/Menu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Prototype.Core.Interfaces;
+
+namespace Prototype.Core.Models
+{
+    public sealed class Menu : IMenu
+    {
+        public Menu(string header, params IMenuItem[] menuItems)
+        {
+            Header = header;
+            Items = new ObservableCollection<IMenuItem>(menuItems);
+        }
+
+        public string Header { get; }
+
+        public IReadOnlyCollection<IMenuItem> Items { get; }
+    }
+
+    public sealed class MenuItem : IMenuItem
+    {
+        public MenuItem(string text, ICommand command)
+        {
+            Text = text;
+            Command = command;
+        }
+
+        public MenuItem(string text, ICommand command, IMenu submenu) : this(text, command)
+        {
+            Submenu = submenu;
+        }
+
+        public string Text { get; }
+
+        public ICommand Command { get; }
+
+        public IMenu Submenu { get; }
+    }
+}
