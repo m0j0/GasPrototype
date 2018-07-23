12452dad041b8efc06d123b1897dc25aaadc5389
m0j0 <maslovn@gmail.com>
2018-07-23 12:34:00 +0300

    minor changes

--- Prototype.Core.View/Controls/PipeFlowScheme/PipeFlowCanvas.cs
+++ Prototype.Core.View/Controls/PipeFlowScheme/PipeFlowCanvas.cs
@@ -45,6 +45,21 @@ namespace Prototype.Core.Controls.PipeFlowScheme
                 _schemeContainer.OnVisualChildrenChanged(addedFlowControl, removedFlowControl);
             }
         }
+        protected override System.Windows.Size MeasureOverride(System.Windows.Size constraint)
+        {
+            base.MeasureOverride(constraint);
+            double width = base
+                .InternalChildren
+                .OfType<UIElement>()
+                .Max(i => i.DesiredSize.Width + (double)i.GetValue(Canvas.LeftProperty));
+
+            double height = base
+                .InternalChildren
+                .OfType<UIElement>()
+                .Max(i => i.DesiredSize.Height + (double)i.GetValue(Canvas.TopProperty));
+
+            return new Size(width, height);
+        }
 
         private void OnLoaded(object sender, RoutedEventArgs e)
         {
