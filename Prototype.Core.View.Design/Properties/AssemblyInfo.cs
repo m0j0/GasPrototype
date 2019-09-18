64b436e69a556f3a1726eb7237b1be01af1401f3
m0j0 <maslovn@gmail.com>
2019-09-18 16:22:50 +0300

    migrate to .net core 3

--- Prototype.Core.View.Design/Properties/AssemblyInfo.cs
+++ Prototype.Core.View.Design/Properties/AssemblyInfo.cs
@@ -4,18 +4,6 @@ using System.Runtime.InteropServices;
 using Microsoft.Windows.Design.Metadata;
 using Prototype.Core.Design;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Prototype.Core.View.Design")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Applied Materials")]
-[assembly: AssemblyProduct("Prototype.Core.View.Design")]
-[assembly: AssemblyCopyright("Copyright © Applied Materials 2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
@@ -23,18 +11,5 @@ using Prototype.Core.Design;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("25fc193d-a209-4284-9202-c669ef8416ae")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: ProvideMetadata(typeof(VisualStudioMetadata))]
